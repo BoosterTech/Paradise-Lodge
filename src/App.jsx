22ea8439a7c68f9ac46c6dadc57c62f987084dc2
Darek Podczasik <boostertech@mail.com>
2024-09-29 18:38:00 +0100

    Button-s, Row-s, Input-s added. props applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import GlobalStyles from "./styles/GlobalStyles";
+import { Button } from "./ui/Button";
+import Input from "./ui/Input";
+import Row from "./ui/Row";
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <div>hello world</div>
+      <div>
+        <Row type="horizontal">
+          <h1>The Paradise Lodge</h1>
+          <div>
+            <h2>Check in and out</h2>
+            <Button onClick={() => alert("Check in")}>Check in</Button>
+            <Button
+              variation="secondary"
+              size="small"
+              onClick={() => alert("Check out")}
+            >
+              Check out
+            </Button>
+          </div>
+        </Row>
+        <Row>
+          <h2>Form</h2>
+          <form>
+            <Input type="number" placeholder="Number of guests" />
+            <Input type="number" placeholder="Number of guests" />
+          </form>
+        </Row>
+      </div>
     </>
   );
 }
