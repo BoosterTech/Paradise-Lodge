fac5eb7ed468cd426c580f6c50a9079d7f6c1964
Darek Podczasik <boostertech@mail.com>
2024-09-29 14:15:49 +0100

    Global Styles created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-const App = () => {
-  return <div>hello world</div>;
-};
+import GlobalStyles from "./styles/GlobalStyles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <div>hello world</div>
+    </>
+  );
+}
 
 export default App;
