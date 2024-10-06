2cf270503295db39623cb5b95509dbc3c42dd603
Darek Podczasik <boostertech@mail.com>
2024-10-06 11:33:54 +0100

    createCabin function created

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -11,6 +11,22 @@ export async function getCabins() {
   return data;
 }
 
+export async createcabit(NewCabin){
+  
+const { data, error } = await supabase
+.from('cabins')
+.insert([
+  { some_column: 'someValue', other_column: 'otherValue' },
+])
+.select()
+
+if(error){
+  console.error(error);
+  throw new Error("Cabin could not be created");
+}
+
+}
+
 export async function deleteCabin(id) {
   const { data, error } = await supabase.from("cabins").delete().eq("id", id);
 
