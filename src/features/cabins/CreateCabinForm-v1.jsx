2fa618f08cd2c3b80a57b37a411825270fe215ec
Darek Podczasik <boostertech@mail.com>
2024-10-12 17:34:00 +0100

    <CreateCabinForm> duplicate file created. <CreateCabinForm> linked to "edit" button for display. <CreateCabinForm> prop-types  added. <CreateCabinForm> defaultValues added

---
+++ src/features/cabins/CreateCabinForm-v1.jsx
@@ -0,0 +1,150 @@
+import toast from "react-hot-toast";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import PropTypes from "prop-types";
+
+import Input from "../../ui/Input";
+import Form from "../../ui/Form";
+import { Button } from "../../ui/Button";
+import FileInput from "../../ui/FileInput";
+import Textarea from "../../ui/Textarea";
+import FormRow from "../../ui/FormRow";
+
+import { createCabin } from "../../services/apiCabins";
+import { useForm } from "react-hook-form";
+
+function CreateCabinForm({ cabinToEdit = {} }) {
+  const { id: editId, ...editValues } = cabinToEdit;
+  const isEditSession = Boolean(editId);
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    getValues,
+    formState: { errors },
+  } = useForm({ defaultValues: isEditSession ? editValues : {} });
+
+  const queryClient = useQueryClient();
+
+  const { mutate, isLoading: isCreating } = useMutation({
+    mutationFn: createCabin,
+    onSuccess: () => {
+      toast.success("New cabin successfully created");
+      queryClient.invalidateQueries({ queryKey: ["cabins"] });
+      reset();
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    },
+  });
+
+  function onSubmit(data) {
+    mutate({ ...data, image: data.image[0] });
+  }
+
+  function onError(errors) {
+    console.log(errors);
+  }
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit, onError)}>
+      <FormRow label="Cabin name" error={errors?.name?.message}>
+        <Input
+          type="text"
+          id="name"
+          autoComplete="name" // Autocomplete attribute
+          disabled={isCreating}
+          {...register("name", { required: "This field is required" })}
+        />
+      </FormRow>
+
+      <FormRow label="Maximum capacity" error={errors?.maxCapacity?.message}>
+        <Input
+          type="number"
+          id="maxCapacity"
+          autoComplete="off"
+          disabled={isCreating}
+          {...register("maxCapacity", {
+            required: "This field is required",
+            min: {
+              value: 1,
+              message: "Capacity should be at least 1",
+            },
+          })}
+        />
+      </FormRow>
+
+      <FormRow label="Regular price" error={errors?.regularPrice?.message}>
+        <Input
+          type="number"
+          id="regularPrice"
+          autoComplete="off"
+          disabled={isCreating}
+          {...register("regularPrice", { required: "This field is required" })}
+        />
+      </FormRow>
+
+      <FormRow label="Discount" error={errors?.discount?.message}>
+        <Input
+          type="number"
+          id="discount"
+          disabled={isCreating}
+          defaultValue={0}
+          {...register("discount", {
+            required: "This field is required",
+            validate: (value) => {
+              const discount = parseFloat(value);
+              const regularPrice = parseFloat(getValues().regularPrice);
+
+              if (!regularPrice) {
+                return "Regular price is required before applying a discount.";
+              }
+
+              return (
+                discount <= regularPrice ||
+                "Discount should be less that regular price"
+              );
+            },
+          })}
+        />
+      </FormRow>
+
+      <FormRow
+        label="Description for website"
+        error={errors?.description?.message}
+      >
+        <Textarea
+          id="description"
+          defaultValue=""
+          disabled={isCreating}
+          {...register("description", { required: "This field is required" })}
+        />
+      </FormRow>
+
+      <FormRow label="Cabin Photo">
+        <FileInput
+          type="file"
+          id="image"
+          accept="image/*"
+          {...register("image", { required: "This field is required" })}
+        />
+      </FormRow>
+
+      <FormRow>
+        {/* type is an HTML attribute! */}
+        <Button $variation="secondary" type="reset">
+          Cancel
+        </Button>
+        <Button disabled={isCreating} type="submit">
+          Add cabin
+        </Button>
+      </FormRow>
+    </Form>
+  );
+}
+
+CreateCabinForm.propTypes = {
+  cabinToEdit: PropTypes.object,
+};
+
+export default CreateCabinForm;
