6e9041fff8439f2271dc1fa4fb768701c7db0a66
Darek Podczasik <boostertech@mail.com>
2024-10-19 12:26:41 +0100

    component wrapped in <div>

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,26 +1,27 @@
 import { Button } from "../../ui/Button";
-
 import Modal from "../../ui/Modal";
 import CabinTable from "./CabinTable";
 import CreateCabinForm from "./CreateCabinForm-v1";
 
 function AddCabin() {
   return (
-    <Modal>
-      <Modal.Open opens="cabin-form">
-        <Button>Add new cabin</Button>
-      </Modal.Open>
-      <Modal.Window name="cabin-form">
-        <CreateCabinForm />
-      </Modal.Window>
+    <div>
+      <Modal>
+        <Modal.Open opens="cabin-form">
+          <Button>Add new cabin</Button>
+        </Modal.Open>
+        <Modal.Window name="cabin-form">
+          <CreateCabinForm />
+        </Modal.Window>
 
-      <Modal.Open opens="table">
-        <Button>Show table</Button>
-      </Modal.Open>
-      <Modal.Window name="table">
-        <CabinTable />
-      </Modal.Window>
-    </Modal>
+        <Modal.Open opens="table">
+          <Button>Show table</Button>
+        </Modal.Open>
+        <Modal.Window name="table">
+          <CabinTable />
+        </Modal.Window>
+      </Modal>
+    </div>
   );
 }
 
