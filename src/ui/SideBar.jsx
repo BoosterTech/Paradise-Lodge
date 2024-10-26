54b1ba8b1f8e25dd8bc2063b589b2499cd8f1e64
Darek Podczasik <boostertech@mail.com>
2024-10-26 21:11:24 +0100

    apply <Uploader/> in <SideBar/>

--- src/ui/SideBar.jsx
+++ src/ui/SideBar.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Logo from "../ui/Logo";
 import MainNav from "../ui/MainNav";
+import Uploader from "../data/Uploader";
 
 const StyledSideBar = styled.aside`
   background-color: var(--color-grey-0);
@@ -18,6 +19,7 @@ const SideBar = () => {
     <StyledSideBar>
       <Logo />
       <MainNav />
+      <Uploader/>
     </StyledSideBar>
   );
 };
