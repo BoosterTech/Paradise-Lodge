10275c3cf50d37e9839ee33674902726915ba759
Darek Podczasik <boostertech@mail.com>
2024-10-27 13:13:43 +0000

    fix pagination "to" number displayed when on last page

--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -1,6 +1,7 @@
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
 import { useSearchParams } from "react-router-dom";
 import styled from "styled-components";
+import { PAGE_SIZE } from "../utils/constants";
 
 const StyledPagination = styled.div`
   width: 100%;
@@ -58,8 +59,6 @@ const PaginationButton = styled.button`
   }
 `;
 
-const PAGE_SIZE = 5;
-
 const Pagination = ({ count }) => {
   const [searchParams, setSearchParams] = useSearchParams();
   const currentPage = !searchParams.get("page")
@@ -85,7 +84,10 @@ const Pagination = ({ count }) => {
     <StyledPagination>
       <P>
         Showing <span>{(currentPage - 1) * PAGE_SIZE + 1}</span> to
-        <span>{currentPage * PAGE_SIZE}</span> of<span>{count}</span> results
+        <span>
+          {currentPage === pageCount ? count  : currentPage * PAGE_SIZE}
+        </span>{" "}
+        of<span>{count}</span> results
       </P>
       <Buttons>
         <PaginationButton onClick={prevPage} disabled={currentPage === 1}>
