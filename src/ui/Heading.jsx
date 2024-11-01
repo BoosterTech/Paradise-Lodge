1fd2e5d8b39460fea240f1cfbdc0f7753bac3a83
Darek Podczasik <boostertech@mail.com>
2024-11-01 21:44:45 +0000

    add Heading styles

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -1,5 +1,21 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-const Heading = styled.h1``;
+const Heading = styled.h1`
+  font-size: 3rem;
+
+  ${(props) =>
+    props.as === "h3" &&
+    css`
+      font-size: 2rem;
+      text-align: center;
+    `}
+
+  ${(props) =>
+    props.as === "h4" &&
+    css`
+      font-size: 3rem;
+      text-align: center;
+    `}
+`;
 
 export default Heading;
