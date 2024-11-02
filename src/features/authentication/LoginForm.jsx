f83f54004d992159297a0708194b48bdf782e577
Darek Podczasik <boostertech@mail.com>
2024-11-02 16:31:08 +0000

    define handleSubmit function for login form. use disable of inputs and button when isLoading data apply mini spinner to submit button when isLoading

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -1,14 +1,23 @@
 import { useState } from "react";
-import {Button} from "../../ui/Button";
+import { Button } from "../../ui/Button";
 import Form from "../../ui/Form";
 import Input from "../../ui/Input";
 import FormRowVertical from "../../ui/FormRowVertical";
 
-function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+import { useLogin } from "./uselogin";
+import SpinnerMini from "../../ui/SpinnerMini";
 
-  function handleSubmit() {}
+function LoginForm() {
+  const [email, setEmail] = useState("booster@mail.com");
+  const [password, setPassword] = useState("booster");
+  const { login, isLoading } = useLogin();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!email || !password) return;
+
+    login({ email, password });
+  }
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -20,6 +29,7 @@ function LoginForm() {
           autoComplete="username"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          disabled={isLoading}
         />
       </FormRowVertical>
       <FormRowVertical label="Password">
@@ -29,10 +39,13 @@ function LoginForm() {
           autoComplete="current-password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          disabled={isLoading}
         />
       </FormRowVertical>
       <FormRowVertical>
-        <Button size="large">Login</Button>
+        <Button size="large" type="submit" disabled={isLoading}>
+          {!isLoading ? "Login" : <SpinnerMini />}
+        </Button>
       </FormRowVertical>
     </Form>
   );
